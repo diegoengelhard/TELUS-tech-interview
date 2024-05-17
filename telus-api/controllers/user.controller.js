1f34b42d82a057e544a09df54e3575a5a7165bd5
diegoengelhard <00356119@uca.edu.sv>
2024-05-16 19:55:27 -0600

    update: api

--- telus-api/controllers/user.controller.js
+++ telus-api/controllers/user.controller.js
@@ -34,36 +34,36 @@ controller.getUser = async (req, res) => {
     }
 }
 
-// METHOD 2 - Redis-fetch: fetch user by id
-controller.getUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        let json;
+// METHOD 2 - Redis-fetch: fetch user by id (uncomment to use)
+// controller.getUser = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         let json;
 
-        // Check if data is in Redis cache
-        client.get(id, async (err, result) => {
-            if (err) throw err;
+//         // Check if data is in Redis cache
+//         client.get(id, async (err, result) => {
+//             if (err) throw err;
 
-            if (result) {
-                console.log('Getting info from Redis cache...');
-                json = JSON.parse(result);
-            } else {
-                console.log('Fetching from https://jsonplaceholder.typicode.com/users/ since its the first time...');
-                const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-                json = await response.json();
+//             if (result) {
+//                 console.log('Getting info from Redis cache...');
+//                 json = JSON.parse(result);
+//             } else {
+//                 console.log('Fetching from https://jsonplaceholder.typicode.com/users/ since its the first time...');
+//                 const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+//                 json = await response.json();
 
-                // Save data to Redis cache
-                client.setex(id, 3600, JSON.stringify(json));
-            }
+//                 // Save data to Redis cache
+//                 client.setex(id, 3600, JSON.stringify(json));
+//             }
 
-            console.log(json);
-            res.status(200).send(json);
-        });
+//             console.log(json);
+//             res.status(200).send(json);
+//         });
         
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// }
 
 module.exports = controller;
